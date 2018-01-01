f0c4c941323f25a3f20bf9feb22b8610037e863a
Christopher Brown <chbphone55@gmail.com>
2018-01-01 16:04:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 
-document.addEventListener( 'load', () => {
-  function smoothScrollTo( element ) {
-    element.scrollIntoView( { behavior: 'smooth' } )
-  }
+function smoothScrollTo( element ) {
+  element.scrollIntoView( { behavior: 'smooth', block: 'start' } )
+}
+
+document.addEventListener( 'DOMContentLoaded', () => {
   
   learnmore.addEventListener( 'click', () => {
     smoothScrollTo( expertise )
   })
+  
+  availableForHireBanner.addEventListener( 'click', () => {
+    if ( getComputedStyle( availableForHireBanner ).backgroundColor === "rgba(255, 182, 131, 0.9)" )
+      smoothScrollTo( hireForm )
+  })
+  
 })
