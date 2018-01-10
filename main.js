fcff106b0b5679470b5c41513709f1a07153ea91
Christopher Brown <chbphone55@gmail.com>
2018-01-10 00:01:13 -0700

    Start section#expertise and new Header
    The `header` has received a new background as well as a removal of the selfie replaced with some text and a nice button.
    The section `#expertise` has received the js animated typing text.

--- main.js
+++ main.js
@@ -8,17 +8,66 @@ document.addEventListener( 'DOMContentLoaded', () => {
     expertise.smoothScrollIntoView( 'easeInOutExpo', 1500, 'start' )
   })
   
-  availableForHireBanner.addEventListener( 'click', () => {
-    if ( getComputedStyle( availableForHireBanner ).backgroundColor.indexOf( '254, 215, 102' ) !== -1 )
-      hireForm.smoothScrollIntoView( 'easeInOutExpo', 1500, 'start' )
+  hireButton.addEventListener( 'click', () => {
+    hireForm.smoothScrollIntoView( 'easeInOutExpo', 1500, 'start' )
+  })
+
+  hireForm.addEventListener( 'keydown', event => {
+  if ( event.key === 'Escape' )
+    event.target.blur()
   })
   
   Array.from( document.querySelectorAll( '.float-input > input, .float-input > textarea' ) )
     .forEach( el => el.addEventListener( 'input', checkForInput ) | checkForInput( { target: el } ) )
   
+  Array.from( phrases.children ).forEach( el => {
+    el.getAttribute( 'value' ).split( '' ).forEach( letter => {
+      const child = document.createElement( 'span' )
+      child.innerText = letter
+      el.appendChild( child )
+    })
+  })
+
+  animatePhrases()
 })
 
-document.addEventListener( 'keydown', event => {
-  if ( event.key === 'Escape' )
-    event.target.blur()
-})
+function animatePhrases() {
+  const phraseList = phrases.children
+
+  let phraseListIndex = phraseLetterIndex = 0,
+      phraseLetterList = phraseList[ phraseListIndex ].children
+
+  function clearPhrase( ) {
+    // Select Phrase
+    phraseList[ phraseListIndex ].classList.remove( 'blink' )
+    phraseList[ phraseListIndex ].classList.add( 'selected' )
+    phraseList[ phraseListIndex ].classList.add( 'hide-cursor' )
+    setTimeout( () => {
+      // Clear Phrase and input Next one
+      Array.from( phraseLetterList ).forEach( letter => letter.classList.remove( 'on' ) )
+      phraseLetterIndex = 0
+      phraseList[ phraseListIndex ].classList = ''
+      if ( ++phraseListIndex >= phraseList.length )
+        phraseListIndex = 0
+      phraseList[ phraseListIndex ].classList.add( 'active' )
+      phraseLetterList = phraseList[ phraseListIndex ].children
+      setTimeout( animatePhrase, 80 )
+    }, 200 )
+  }
+
+  function animatePhrase() {
+
+    // Animate Here
+    phraseLetterList[ phraseLetterIndex ].classList.add( 'on' )
+
+    // Next Letter / Phrase
+    if ( ++phraseLetterIndex >= phraseLetterList.length ) {
+        phraseList[ phraseListIndex ].classList.add( 'blink' )
+        setTimeout( clearPhrase, 2000 )
+    } else {
+      setTimeout( animatePhrase, 65 )
+    }
+  }
+
+  animatePhrase()
+}
