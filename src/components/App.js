3753c6ee29d28719611b3e8d75b9838b7078c98e
Christopher Brown <chbphone55@gmail.com>
2018-07-21 05:07:33 -0600

    Add in screenshots to slides, and style slides + style hire section

--- src/components/App.js
+++ src/components/App.js
@@ -169,33 +169,84 @@ const ChallengeSection = () => (
   </section>
 )
 
+
+import weatherCodepen from '../../assets/screenshots/WeatherCodepen.png'
+import stanagerCarbon from '../../assets/screenshots/StanagerCarbon.png'
+import landingPage from '../../assets/screenshots/ProductLandingPageCodepen.png'
+import pomodoro from '../../assets/screenshots/PomodoroCodepen.png'
+import iacsCounselors from '../../assets/screenshots/IACS_Counselors.png'
+import franciscanFaculty from '../../assets/screenshots/FranciscanFaculty.png'
+import waveCodepen from '../../assets/screenshots/AnimateWaveSVG_Codepen.png'
+
+const workLink = ({file, alt, description, url}) => (
+  <article className={style.slideItem}>
+    <img src={file} alt={alt} className={style.slideImage} />
+    <div className={style.slideContent}>
+      <p>{description}</p>
+      <a href={url}>Explore</a>
+    </div>
+  </article>
+)
+
+const slides = [
+  [
+    workLink({
+      file: weatherCodepen,
+      alt: 'web app built in codepen that shows the weather and forecast',
+      description: 'A simple, elegant web app designed to deliver the weather and forecast of your location.',
+      url: 'https://codepen.io/chbchb55/full/pdNKEB/'
+    }),
+    workLink({
+      file: stanagerCarbon,
+      alt: 'screenshot of code for the library stanager via carbon',
+      description: 'A library I created named "Stanager", it is an immutable state manager with subscribe and unsubscribe features.',
+      url: 'https://npm.im/stanager'
+    }),
+    workLink({
+      file: franciscanFaculty,
+      alt: 'screenshot of Franciscan University\'s faculty page',
+      description: 'I helped design and build several of the pages for Franciscan University\'s website. You can look at the pull request in the link below, or view the rest of my contributions on their repository.',
+      url: 'https://github.com/fus-marcom/franciscan-react/pull/146'
+    })
+  ], [
+    workLink({
+      file: iacsCounselors,
+      alt: 'screenshot of IACS Counselor\'s page',
+      description: 'A static mobile-friendly website designed, content edited, and built from the ground up by me, for the counselors of IACS.',
+      url: 'https://iacs-counselor.github.io/'
+    }),
+    workLink({
+      file: pomodoro,
+      alt: 'screenshot of a pomodoro clock',
+      description: 'A freshly designed, functional and responsive Pomodoro clock that adheres to the Pomodoro Technique.',
+      url: 'https://codepen.io/chbchb55/full/ooKdvW/'
+    }),
+    workLink({
+      file: waveCodepen,
+      alt: 'screenshot of article on codepen about animating wave svg',
+      description: 'A stylish article on CodePen that explains how to animate a wave SVG to create an interesting repeating wave effect. Included is a unique (animated) menu icon, and a nice side-drawer style menu.',
+      url: 'https://codepen.io/chbchb55/full/xWoXNG/'
+    })
+  ]
+]
+
 const WorkSection = () => (
-  <section id={ style.mywork }>
-    <h1 className={ style.title }>
+  <section id={style.mywork}>
+    <h1 className={style.title}>
       My Work
     </h1>
-    <Carousel bullets className={ style.slides } options={{
+    <Carousel bullets className={style.slides} options={{
       prevNextButtons: true,
       pageDots: true,
       autoPlay: 5000,
       wrapAround: true,
       arrowShape: 'M 50 0A 1 1 0 0 1 50 100A 1 1 0 0 1 50 0ZM51.13,23.39C53.30,25.55,53.30,29.05,51.14,31.22C48.50,33.87,44.47,37.89,41.83,40.54C40.38,41.99,41.41,44.46,43.45,44.46C51.43,44.46,69.81,44.46,77.79,44.46C80.85,44.46,83.33,46.94,83.33,50.00C83.33,51.11,83.33,48.89,83.33,50.00C83.33,53.06,80.85,55.54,77.79,55.54C69.81,55.54,51.43,55.54,43.46,55.54C41.41,55.54,40.39,58.02,41.83,59.46C44.48,62.11,48.50,66.12,51.15,68.77C53.31,70.93,53.31,74.44,51.14,76.59C50.34,77.39,51.88,75.86,51.08,76.65C48.91,78.81,45.40,78.80,43.24,76.63C37.92,71.31,25.90,59.25,20.58,53.92C18.42,51.75,18.42,48.25,20.58,46.08C25.90,40.75,37.92,28.69,43.24,23.36C45.40,21.19,48.91,21.19,51.08,23.34C51.88,24.13,50.33,22.60,51.13,23.39Z'
     }}>
-      <article className={ style.slide }>
-        <img src='https://i.imgur.com/8lEJtbg.jpg' />
-        <img src='https://i.imgur.com/8lEJtbg.jpg' />
-        <img src='https://i.imgur.com/8lEJtbg.jpg' />
-      </article>
-        <article className={ style.slide }>
-        <img src='https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/indian-peafowl-closeup.ngsversion.1396531041348.jpg' />
-        <img src='https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/indian-peafowl-closeup.ngsversion.1396531041348.jpg' />
-        <img src='https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/indian-peafowl-closeup.ngsversion.1396531041348.jpg' />
-      </article>
-      <article className={ style.slide }>
-        <img src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-female-african-elephant-loxodonta-joel-sartore.jpg' />
-        <img src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-female-african-elephant-loxodonta-joel-sartore.jpg' />
-        <img src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-female-african-elephant-loxodonta-joel-sartore.jpg' />
-      </article> 
+      {slides.map(links => (
+        <section className={style.slide}>
+          {links}
+        </section>
+      ))}
     </Carousel>
   </section>
 )
